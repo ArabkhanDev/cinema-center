e8dbe8fe7bda4ce9784a028cf34e0c666a8060dd
arabxanrmzv <arabkhanramazanov@gmail.com>
2024-07-15 19:16:46 +0400

    fix: solve bugs

--- src/main/java/group/aist/cinema/mapper/FavoriteMapper.java
+++ src/main/java/group/aist/cinema/mapper/FavoriteMapper.java
@@ -3,7 +3,6 @@ package group.aist.cinema.mapper;
 import group.aist.cinema.dto.request.FavoriteRequestDTO;
 import group.aist.cinema.dto.response.FavoriteResponseDTO;
 import group.aist.cinema.model.Favorite;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
--- src/main/java/group/aist/cinema/repository/FavoriteRepository.java
+++ src/main/java/group/aist/cinema/repository/FavoriteRepository.java
@@ -6,16 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
    @EntityGraph(attributePaths = {"movies", "user"})
    Page<Favorite> findAllBy(Pageable pageable);
-
-   @EntityGraph(attributePaths = {"movies", "user"})
-   List<Favorite> findAll();
 
    @EntityGraph(attributePaths = {"movies", "user"})
    Page<Favorite> findAllByUserId(Long userId, Pageable pageable);
