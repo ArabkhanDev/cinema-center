0da3cdeb45abc00153cbde7b8c0aba663d984b4a
arabxanrmzv <arabkhanramazanov@gmail.com>
2024-07-16 12:53:03 +0400

    feature: add BaseResponse controller layer

--- src/main/java/group/aist/cinema/controller/MovieStreamController.java
+++ src/main/java/group/aist/cinema/controller/MovieStreamController.java
@@ -2,6 +2,7 @@ package group.aist.cinema.controller;
 
 import group.aist.cinema.dto.request.MovieStreamRequestDTO;
 import group.aist.cinema.dto.response.MovieStreamResponseDTO;
+import group.aist.cinema.model.base.BaseResponse;
 import group.aist.cinema.service.MovieStreamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -16,37 +17,40 @@ public class MovieStreamController {
     private final MovieStreamService movieStreamService;
 
     @GetMapping
-    public Page<MovieStreamResponseDTO> getAllMovieStreams(Pageable pageable) {
-        return movieStreamService.getAllMovieStreams(pageable);
+    public BaseResponse<Page<MovieStreamResponseDTO>> getAllMovieStreams(Pageable pageable) {
+        return BaseResponse.success(movieStreamService.getAllMovieStreams(pageable));
     }
 
     @GetMapping("/{id}")
-    public MovieStreamResponseDTO getMovieStreamById(@PathVariable Long id) {
-        return movieStreamService.getMovieStreamById(id);
+    public BaseResponse<MovieStreamResponseDTO> getMovieStreamById(@PathVariable Long id) {
+        return BaseResponse.success(movieStreamService.getMovieStreamById(id));
     }
 
     @PostMapping
-    public MovieStreamResponseDTO createMovieStream(@RequestBody MovieStreamRequestDTO movieStreamRequestDto) {
-        return movieStreamService.createMovieStream(movieStreamRequestDto);
+    public BaseResponse<MovieStreamResponseDTO> createMovieStream(@RequestBody MovieStreamRequestDTO movieStreamRequestDto) {
+        return BaseResponse.created(movieStreamService.createMovieStream(movieStreamRequestDto));
     }
 
     @PutMapping("/{id}")
-    public MovieStreamResponseDTO updateMovieStream(@PathVariable Long id, @RequestBody MovieStreamRequestDTO movieStreamRequestDto) {
-        return movieStreamService.updateMovieStream(id, movieStreamRequestDto);
+    public BaseResponse<MovieStreamResponseDTO> updateMovieStream(@PathVariable Long id, @RequestBody MovieStreamRequestDTO movieStreamRequestDto) {
+        return BaseResponse.success(movieStreamService.updateMovieStream(id, movieStreamRequestDto));
     }
 
     @PostMapping("/{streamId}/dubbing/{dubbingId}")
-    public void addDubbingLanguage(@PathVariable Long streamId, @PathVariable Long dubbingId) {
+    public BaseResponse<Void> addDubbingLanguage(@PathVariable Long streamId, @PathVariable Long dubbingId) {
         movieStreamService.addDubbingLanguageToMovieStream(streamId, dubbingId);
+        return BaseResponse.created();
     }
 
     @PostMapping("/{streamId}/subtitle/{subtitleId}")
-    public void addSubtitleLanguage(@PathVariable Long streamId, @PathVariable Long subtitleId) {
+    public BaseResponse<Void> addSubtitleLanguage(@PathVariable Long streamId, @PathVariable Long subtitleId) {
         movieStreamService.addSubtitleLanguageToMovieStream(streamId, subtitleId);
+        return BaseResponse.created();
     }
 
     @DeleteMapping("/{id}")
-    public void deleteMovieStream(@PathVariable Long id) {
+    public BaseResponse<Void> deleteMovieStream(@PathVariable Long id) {
         movieStreamService.deleteMovieStream(id);
+        return BaseResponse.noContent();
     }
 }
--- src/main/java/group/aist/cinema/controller/UserController.java
+++ src/main/java/group/aist/cinema/controller/UserController.java
@@ -3,15 +3,12 @@ package group.aist.cinema.controller;
 import group.aist.cinema.dto.request.UserRequestDTO;
 import group.aist.cinema.dto.request.UserUpdateRequest;
 import group.aist.cinema.dto.response.UserResponseDTO;
+import group.aist.cinema.model.base.BaseResponse;
 import group.aist.cinema.service.UserService;
-import group.aist.cinema.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 
 @RestController
@@ -22,33 +19,34 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public Page<UserResponseDTO> getAllUsers(Pageable pageable) {
-        return userService.getAllUsers(pageable);
+    public BaseResponse<Page<UserResponseDTO>> getAllUsers(Pageable pageable) {
+        return BaseResponse.success(userService.getAllUsers(pageable));
     }
 
     @GetMapping("/{id}")
-    public UserResponseDTO getUserById(@PathVariable String id) {
-        return userService.getUserById(id);
+    public BaseResponse<UserResponseDTO> getUserById(@PathVariable String id) {
+        return BaseResponse.success(userService.getUserById(id));
     }
 
     @GetMapping("/email")
-    public UserResponseDTO getUserByEmail(@RequestParam String email) {
-        return userService.getUserByEmail(email);
+    public BaseResponse<UserResponseDTO> getUserByEmail(@RequestParam String email) {
+        return BaseResponse.success(userService.getUserByEmail(email));
     }
 
     @PostMapping
-    public UserResponseDTO registerUser(@RequestBody UserRequestDTO userDTO) {
-        return userService.registerUser(userDTO);
+    public BaseResponse<UserResponseDTO> registerUser(@RequestBody UserRequestDTO userDTO) {
+        return BaseResponse.success(userService.registerUser(userDTO));
     }
 
     @PutMapping("/{id}")
-    public UserResponseDTO updateUser(@PathVariable String id, @RequestBody UserUpdateRequest userDTO) {
-        return userService.updateUser(id, userDTO);
+    public BaseResponse<UserResponseDTO> updateUser(@PathVariable String id, @RequestBody UserUpdateRequest userDTO) {
+        return BaseResponse.success(userService.updateUser(id, userDTO));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable String id) {
+    public BaseResponse<Void> deleteUser(@PathVariable String id) {
         userService.deleteUserById(id);
+        return BaseResponse.noContent();
     }
 
 }
