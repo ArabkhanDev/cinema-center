8d169a20625de7c5557b5290dabd407f06e47e5c
ElshanDunyamaliyev <elshandunyamaliyev@gmail.com>
2024-07-17 17:21:32 +0400

    feature: added validaton to dtos

--- src/main/java/group/aist/cinema/dto/request/UserUpdateRequest.java
+++ src/main/java/group/aist/cinema/dto/request/UserUpdateRequest.java
@@ -1,5 +1,7 @@
 package group.aist.cinema.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Data
@@ -7,8 +9,10 @@ import lombok.*;
 @NoArgsConstructor
 public class UserUpdateRequest {
 
+    @Email(message = "Email must be valid")
     private String email;
 
+    @Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Phone must be a valid number between 10 to 15 digits")
     private String phone;
 
 }
