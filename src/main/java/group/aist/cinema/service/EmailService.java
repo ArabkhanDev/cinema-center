ee5eee14bc5ca3f7b61c12876619ba7ca7a38b17
tarve <tarverdiyevaltun@gmail.com>
2024-07-18 16:41:22 +0400

    JavaMailSender injection

--- src/main/java/group/aist/cinema/service/EmailService.java
+++ src/main/java/group/aist/cinema/service/EmailService.java
@@ -28,7 +28,7 @@ import java.io.IOException;
 public class EmailService {
 
 
-    JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
 
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
